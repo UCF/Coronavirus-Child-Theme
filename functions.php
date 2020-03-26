9f4d90e6ff8c423624fed9b548aa12cc2db32437
Jo Dickson <jogreybill@gmail.com>
2020-03-26 16:44:44 -0400

    Added namespacing to files and theme dir paths to includes in functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,11 @@
 <?php
+namespace Coronavirus\Theme;
+
+define( 'CORONAVIRUS_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
+
 
 // Theme foundation
-include_once 'includes/config.php';
-include_once 'includes/meta.php';
+include_once CORONAVIRUS_THEME_DIR . 'includes/config.php';
+include_once CORONAVIRUS_THEME_DIR . 'includes/meta.php';
 
 // Add other includes to this file as needed.
--- includes/meta.php
+++ includes/meta.php
@@ -3,17 +3,19 @@
  * Includes functions that handle registration/enqueuing of meta tags, styles,
  * and scripts in the document head and footer.
  **/
+namespace Coronavirus\Theme\Includes\Meta;
+
 
 /**
  * Enqueue front-end css and js.
  **/
-function myproject_enqueue_frontend_assets() {
+function enqueue_frontend_assets() {
 	$theme = wp_get_theme();
 	$theme_version = $theme->get( 'Version' );
 
-	wp_enqueue_style( 'style-child', MYPROJECT_THEME_CSS_URL . '/style.min.css', array( 'style' ), $theme_version );
+	wp_enqueue_style( 'style-child', CORONAVIRUS_THEME_CSS_URL . '/style.min.css', array( 'style' ), $theme_version );
 
-	wp_enqueue_script( 'script-child', MYPROJECT_THEME_JS_URL . '/script.min.js', array( 'jquery', 'script' ), $theme_version, true );
+	wp_enqueue_script( 'script-child', CORONAVIRUS_THEME_JS_URL . '/script.min.js', array( 'jquery', 'script' ), $theme_version, true );
 }
 
-add_action( 'wp_enqueue_scripts', 'myproject_enqueue_frontend_assets', 11 );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_frontend_assets', 11 );
