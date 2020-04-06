9013ba1c7930903d443d9d3d801561d67a08fee7
Jim Barnes <jimbarnesdeveloper@gmail.com>
2020-04-06 15:12:25 -0400

    Updated date format

--- includes/structured-data.php
+++ includes/structured-data.php
@@ -14,18 +14,19 @@ function generate_structured_data() {
 		$announcement_text = $announcement_text ?: get_the_excerpt( $post );
 
 		$announcement_expiration = get_field( 'post_expiration_date' );
+		$announcement_expiration = $announcement_expiration ? date_create_from_format( 'm/d/Y', $announcement_expiration ) : null;
 
 		$data = array(
 			'@context' => 'https://schema.org',
 			'@type' => 'SpecialAnnouncement',
 			'name' => $post->post_title,
 			'text' => $announcement_text,
-			'datePosted' => $post->publish_date,
+			'datePosted' => $post->post_date,
 			'category' => 'https://www.wikidata.org/wiki/Q81068910'
 		);
 
 		if ( $announcement_expiration ) {
-			$data['expires'] = $announcement_expiration;
+			$data['expires'] = $announcement_expiration->format( 'Y-m-d H:j:s' );
 		}
 
 	?>
